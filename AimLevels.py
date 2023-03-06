39b28b084aab547bd12cc84e7eb0b542ce484ab7
MeffyS <patryk.czop@gmail.com>
2023-03-06 04:09:30 +0100

    Add click sound | Add miss sound

--- AimLevels.py
+++ AimLevels.py
@@ -41,23 +41,11 @@ class Level_0(AimLevel):
         # return (self.level + self.min_points) + 10
 
 
-def points_checker(points):
-    a_level = Level_0()
-    if points >= 10 and points <= 100:
-        a_level = Level_1()
-        print(a_level.level)
-
-    if points >= 101 and points < 151:
-        a_level = Level_2()
-
-    return a_level
-
-
 class Level_1(AimLevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.level = 1
-        self.change_speed = 9700
+        self.change_speed = 500
 
     def money_converter(self):
         pass
@@ -72,7 +60,7 @@ class Level_2(AimLevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.level = 2
-        self.change_speed = 9400
+        self.change_speed = 500
 
     def money_converter(self):
         pass
@@ -87,7 +75,7 @@ class Level_3(AimLevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.level = 3
-        self.change_speed = 9000
+        self.change_speed = 500
 
     def money_converter(self):
         pass
@@ -96,3 +84,132 @@ class Level_3(AimLevel):
     def count_points(self):
         pass
         # return (self.level + self.min_points) + 20
+
+
+class Level_4(AimLevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.level = 4
+        self.change_speed = 500
+
+    def money_converter(self):
+        pass
+        # return (self.level + 0) * 2.5
+
+    def count_points(self):
+        pass
+        # return (self.level + self.min_points) + 20
+
+
+class Level_5(AimLevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.level = 5
+        self.change_speed = 500
+
+    def money_converter(self):
+        pass
+        # return (self.level + 0) * 2.5
+
+    def count_points(self):
+        pass
+        # return (self.level + self.min_points) + 20
+
+
+class Level_6(AimLevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.level = 6
+        self.change_speed = 7500
+
+    def money_converter(self):
+        pass
+        # return (self.level + 0) * 2.5
+
+    def count_points(self):
+        pass
+        # return (self.level + self.min_points) + 20
+
+
+class Level_7(AimLevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.level = 7
+        self.change_speed = 6900
+
+    def money_converter(self):
+        pass
+        # return (self.level + 0) * 2.5
+
+    def count_points(self):
+        pass
+        # return (self.level + self.min_points) + 20
+
+
+class Level_8(AimLevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.level = 8
+        self.change_speed = 6400
+
+    def money_converter(self):
+        pass
+        # return (self.level + 0) * 2.5
+
+    def count_points(self):
+        pass
+        # return (self.level + self.min_points) + 20
+
+
+class Level_9(AimLevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.level = 9
+        self.change_speed = 5900
+
+    def money_converter(self):
+        pass
+        # return (self.level + 0) * 2.5
+
+    def count_points(self):
+        pass
+        # return (self.level + self.min_points) + 20
+
+
+class Level_10(AimLevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.level = 10
+        self.change_speed = 5400
+
+    def money_converter(self):
+        pass
+        # return (self.level + 0) * 2.5
+
+    def count_points(self):
+        pass
+        # return (self.level + self.min_points) + 20
+
+
+def points_checker(points):
+    a_level = Level_0()
+    levels = {
+        "Level_0()": [0, 10],
+        "Level_1()": [11, 20],
+        'Level_2()':[21,30],
+        'Level_3()':[31,40],
+        'Level_4()':[41,50],
+        'Level_5()':[51,60],
+        'Level_6()':[61,70],
+        'Level_7()':[71,80],
+        'Level_8()':[81,90],
+        'Level_9()':[91,100],
+        'Level_10()':[101,110]
+    }
+    for level, determinant in levels.items():
+        if determinant[0] <= points and determinant[1] >= points:
+            a_level = eval(level)
+    return a_level
+
+a = points_checker(120)
+print(a.level)
--- AimPlay.py
+++ AimPlay.py
@@ -4,14 +4,16 @@ import sys
 import random
 import time
 
-from abc import ABC
+
+from PySide6.QtCore import QUrl
+from PySide6.QtMultimedia import QSoundEffect
 
 from PySide6.QtCore import QTimer, Slot, QDateTime, QElapsedTimer
-
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtMultimedia
 from PySide6.QtWidgets import QApplication, QPushButton, QLabel
-from PySide6.QtGui import QPixmap
+from PySide6.QtGui import QPixmap, QCursor
 
+from abc import ABC
 import AimLevels
 
 
@@ -19,14 +21,14 @@ class Play(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-       
-
         self.hearts()
         self.setFixedSize(1280, 800)
+        # self.setCursor(QCursor(ClosedHandCursor))
 
         self.aim_seconds = 0
         self.points = 0
-
+        self.hit_points_value = 0
+        self.miss_points_value = 0
 
         self.aim_timer_seconds = QLabel("0", self)
         self.aim_timer_seconds.setGeometry(145, 15, 300, 25)
@@ -44,7 +46,21 @@ class Play(QtWidgets.QWidget):
         self.aim_points.setGeometry(525, 15, 300, 25)
         self.aim_points.setStyleSheet("font-size: 25px;")
 
-        self.obj_first = AimObject(100, 100, 50, 50, "obj_1", 5)
+        self.hit_object_label = QLabel(f"Hit: {self.hit_points_value}", self)
+        self.hit_object_label.setGeometry(1000, 15, 300, 25)
+        self.hit_object_label.setStyleSheet("font-size: 25px;")
+
+        self.miss_object_label = QLabel(f"Miss: {self.miss_points_value}", self)
+        self.miss_object_label.setGeometry(1100, 15, 300, 25)
+        self.miss_object_label.setStyleSheet("font-size: 25px;")
+
+        self.obj_first = AimObject(100, 100, 200, 200, "obj_1", 5)
+
+        self.hit_effect = QSoundEffect(self)
+        self.hit_effect.setSource(QUrl.fromLocalFile("Aim_audio/punch.wav"))
+
+        self.fail_effect = QSoundEffect(self)
+        self.fail_effect.setSource(QUrl.fromLocalFile("Aim_audio/fail_sound.wav"))
 
         self.obj_first.setGeometry(
             self.obj_first.position_x,
@@ -70,7 +86,9 @@ class Play(QtWidgets.QWidget):
         self.start_time = QDateTime.currentDateTime()
 
     def no_clicked_button_change_position(self):
+        self.fail_effect.play()
         self.remove_points()
+        self.miss_points()
 
         self.new_position_x = random.randint(20, 600)
         self.new_position_y = random.randint(20, 600)
@@ -82,7 +100,9 @@ class Play(QtWidgets.QWidget):
         )
 
     def clicked_button_change_position(self):
+        self.hit_effect.play()
         self.add_points()
+        self.hit_points()
 
         self.new_position_x = random.randint(20, 600)
         self.new_position_y = random.randint(20, 600)
@@ -92,6 +112,7 @@ class Play(QtWidgets.QWidget):
             self.obj_first.o_width,
             self.obj_first.o_height,
         )
+        
 
     def update_time_label(self):
 
@@ -119,21 +140,22 @@ class Play(QtWidgets.QWidget):
         self.points += 1
         self.aim_points.setText(f"Points: {self.points}")
         self.new_level = AimLevels.points_checker(self.points)
-
         self.aim_level.setText(str(self.new_level.level))
-        print(self.new_level.level)
         self.timer.start(self.new_level.change_speed)
-        # print(self.new_level.change_speed)
-        # print(type(self.new_level.change_speed))
-
-        
 
     def remove_points(self):
         self.points -= 1
         self.aim_points.setText(f"Points: {self.points}")
         self.new_level = AimLevels.points_checker(self.points)
         self.aim_level.setText(str(self.new_level.level))
-        # self.timer.start(self.new_level.change_speed)
+
+    def hit_points(self):
+        self.hit_points_value += 1
+        self.hit_object_label.setText(f"Hit: {self.hit_points_value}")
+
+    def miss_points(self):
+        self.miss_points_value += 1
+        self.miss_object_label.setText(f"Miss: {self.miss_points_value}")
 
 
 class AimObject(QtWidgets.QPushButton):
