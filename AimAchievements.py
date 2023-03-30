723b463ad1a9811a45f01dba642733566d526d72
patryk.czop@gmail.com <patryk.czop.business@gmail.com>
2023-03-30 21:49:37 +0200

    Work on aim Achievements

---
+++ AimAchievements.py
@@ -0,0 +1,108 @@
+import sys
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QLabel,
+    QLineEdit,
+    QTextEdit,
+)
+
+from PySide6.QtCore import QTimer
+from PySide6.QtGui import QScreen, QGuiApplication
+from PySide6 import QtWidgets
+
+achievements = {
+    "achievement_1": ["Get 50 points", "NOT REACHED"],
+    "achievement_2": ["Get 100 points", "NOT REACHED"],
+    "achievement_3": ["Get 200 points", "NOT REACHED"],
+    "achievement_4": ["Get 500 points", "NOT REACHED"],
+    "achievement_5": ["Get 1000 points", "NOT REACHED"],
+}
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setFixedSize(500, 500)
+
+        self.test_label = QLabel("ABC1")
+        self.test_label.hide()
+
+        # self.b = self.my_screen.physicalSize()
+
+        # self.test_label.setGeometry(1210, 270, 50, 50)
+
+        self.my_button = QPushButton("Achievements", self)
+        self.my_button.setGeometry(50, 50, 100, 50)
+
+        self.my_button.enterEvent = self.show_button
+        self.my_button.leaveEvent = self.hide_button
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.refreshMainWindow)
+        self.timer.start(1000)
+
+    def refreshMainWindow(self):
+        self.repaint()
+        self.check_screen_paramters()
+
+    def show_button(self, event):
+        self.check_screen_paramters()
+
+        self.test_label.show()
+
+        super().enterEvent(event)
+
+    def hide_button(self, event):
+        self.test_label.hide()
+        print("no hide")
+        super().leaveEvent(event)
+
+    def check_screen_paramters(self):
+
+        pos = self.pos()
+        x, y = int(pos.x()), int(pos.y())
+        # self.test_label.setGeometry(x + 500, y + 31, 500, 500)
+        self.test_label.setStyleSheet("background-color: #431556; color: #ffffff")
+        achievements_params = self.test_label.geometry()
+        print("achi x", achievements_params.x())
+        print("achi y", achievements_params.y())
+
+        screen = app.primaryScreen()
+        print(f"Screen Height: {screen.size().height()}")
+        print(f"Screen Width: {screen.size().width()}")
+
+        # if screen.size().height() // 2:
+        if achievements_params.x() // 2 > 0:
+            self.test_label.setGeometry(x - 500, y + 31, 500, 500)
+            print(
+                "Lewa",
+            )
+            print(achievements_params.x() // 2, "scr", screen.size().width() // 2)
+            if achievements_params.x() // 2 < 0:
+                self.test_label.setGeometry(x + 500, y + 31, 500, 500) 
+
+                
+        # elif achievements_params.x() // 2 < 0:
+        #     print('p')
+        #     self.test_label.setGeometry(x - 500, y + 31, 500, 500)
+
+        # if achievements_params.x() // 2 < 1:
+        #     print(
+        #         "Prawa",
+        #     )
+        #     self.test_label.setGeometry(x + 400, y + 31, 500, 500)
+        # if achievements_params.x() + 100 > screen.size().height():
+        #     self.test_label.setGeometry(x - 500, y + 31, 500, 500)
+        # if achievements_params.x() - 100 < screen.size().height():
+        #     self.test_label.setGeometry(x - 500, y + 31, 500, 500)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = MainWindow()
+    widget.show()
+    app.exec_()
